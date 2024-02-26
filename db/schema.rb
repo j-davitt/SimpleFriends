3ce7c422576c2e5d0bdf80e477ff9879f657d58a
Joseph Davitt <joseph.j.davitt@gmail.com>
2024-02-26 12:43:25 -0800

    create association between user and friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_184857) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_204109) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_184857) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
